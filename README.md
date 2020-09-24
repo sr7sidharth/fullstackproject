e5a9bb233321cb616e82adbef1a365829604e67a
sr7sidharth <45019932+sr7sidharth@users.noreply.github.com>
2020-09-23 20:07:32 -0700

    styling for skills, wtg and template pages done

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 NOTES:
 1. Note the split of header partials - this allows each page to have additional script tags that need not be used on other pages. 
+
+for coronavirus tracker:
+https://www.npmjs.com/package/axios
+https://documenter.getpostman.com/view/8854915/SzS8rjHv?version=latest#7618a0d5-d412-4360-9548-569645770969
+https://covidtracking.com/
+
+/coronavirusTracker
+search bar: search
+
+/coronavirusTracker/state
+state: str
+graph: 
+    tests: [int] 
+    cases/tested positive: [int]
+    deaths: [int]
+    date:[int] //likely needs to be string
+tracking from: int //likely needs to be string
+updated on: int (date, time) //likely needs to be string
+source: str 
--- seeds.js
+++ seeds.js
@@ -3,19 +3,44 @@ var Skills = require("./models/skill");
 
 var data = [
     {
-        name: "Data Structures", 
-        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        link: "https://www.google.com/"
+        name: "Computer Networks", 
+        image: "https://images.unsplash.com/photo-1598946114934-8cf259aa241d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        link: "http://plaza.eng.uci.edu/course/eecs/148/outline/2017-2018"
     },
     {
-        name: "Desert Mesa", 
-        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
-        link: "https://www.google.com/"
+        name: "Design and use of Database Management Systems using SQL", 
+        image: "https://images.unsplash.com/photo-1597852074816-d933c7d2b988?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        link: "http://catalogue.uci.edu/allcourses/compsci/"
     },
     {
-        name: "Canyon Floor", 
-        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        link: "https://www.google.com/"
+        name: "Proficient at Java, Python, and C++, with limited experience in many more!", 
+        image: "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        link: "http://catalogue.uci.edu/allcourses/compsci/"
+    },
+    {
+        name: "Worked on compilers, interpreters and operating systems",
+        image: "https://images.unsplash.com/photo-1469981283837-561b3779462f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        link: "http://catalogue.uci.edu/allcourses/compsci/"
+    },
+    {
+        name: "Design and analysis of algorithms",
+        image: "https://images.unsplash.com/photo-1574390353491-92705370c72e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1313&q=80",
+        link: "http://catalogue.uci.edu/allcourses/compsci/",
+    },
+    {
+        name: "Design of Embedded systems for various applications",
+        image: "https://images.unsplash.com/photo-1551703616-e5a729ff0185?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80",
+        link: "http://catalogue.uci.edu/allcourses/compsci/"
+    },
+    {
+        name: "Knowledge of Data Structures, their use and implementation",
+        image: "https://images.unsplash.com/photo-1493476523860-a6de6ce1b0c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        link: "http://catalogue.uci.edu/allcourses/compsci/"
+    },
+    {
+        name: "Knowledge of full stack systems - this website was built entirely by me, using the MERN stack!",
+        image: "https://images.unsplash.com/photo-1564760290292-23341e4df6ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        link: "https://en.wikipedia.org/wiki/Web_developer"
     }
 ]
 
